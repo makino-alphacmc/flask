758d1aa3f3ae3bbe27fcea8a4b9260bda72a0627
槇野 <mkjljono@gmail.com>
2026-04-07 17:51:24 +0900

    更新

--- FLASK/app.py
+++ FLASK/app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Hello, World!</h1>'
+    return "<h1>Welcome to the Product Page</h1>"
+
+@app.route('/product')
+def product():
+    product_list =[
+        "computer", "smartphone", "tablet"
+    ]
+    product_dict = {"id": "1", "name": "ノートパソコン", "processor": "core i5", "price": "550000"},
+  
+    return render_template('product.html', product_list=product_list, product_dict=product_dict)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # flaskを用いて学習がてらにHPを実装
-技術スタック：HTML/CSS Bootstrap、Jinja2、python,flask
+
+フロント・サーバーサイド： HTML Python
+CSS： Bootstrap、
+ライブラリ： Jinja2
+フレームワーク: flask
+form: WTForms
